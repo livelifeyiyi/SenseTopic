a1e264d719a7b62ec4b78c33b7c7ee15a46f0517
livelifeyiyi <xiaoya_0922@126.com>
2018-09-14 00:17:29 +0800

    add Y_R_ijt

--- profileEvolution.py
+++ profileEvolution.py
@@ -1,15 +1,20 @@
+import argparse
+
 import numpy as np
+import ConnectDB
 
 
 class ProfileEvolution:
-	def __init__(self):
-		pass
+	def __init__(self, dbip, dbname, pwd, feature_dimension=50):
+		self.D = feature_dimension
+		conDB = ConnectDB.ConnectDB(dbip, dbname, pwd)
+		self.cursor, self.db = conDB.connect_db()
 
 	def cost_function(self):
 		pass
 
 	def minimum_Uit(self, user, item_set, time, gamma, eta, lambda_U):
-		# user i,  time t
+		# user i(id),  time t
 		user_h = []  # ..
 		gamma_i = gamma[user]
 
@@ -20,8 +25,9 @@ class ProfileEvolution:
 			sum_userh += gamma_h * self.L_hit(h, user, time, eta_h) * (self.Uit_hat(h, time + 1) - self.U_it(h, time + 1))
 		min_Uit = 0.0
 		for item in item_set:
+			Y_ijt, R_ijt = self.Y_R_ijt(user, item, time)
 			# item j
-			min_Uit += self.Y_ijt(user, item, time) * (np.dot(U_it, self.V_j(item)) - self.R_ijt(user, item, time)) * self.V_j(item)
+			min_Uit += Y_ijt * (np.dot(U_it, self.V_j(item)) - R_ijt) * self.V_j(item)
 			+ lambda_U*(self.U_it(user, time)-self.Uit_hat(user, time, gamma_i))
 			+ lambda_U*(1-gamma_i)(self.U_it(user, time+1)-self.Uit_hat(user, time+1, gamma_i))
 			+ lambda_U*sum_userh
@@ -56,13 +62,32 @@ class ProfileEvolution:
 			sum_t += (self.Uit_hat(user_i, t, gamma_i) - self.U_it(user_i, t)) * (gamma_i * sum_h + (1-gamma_i) * self.U_it(user_, t-1))
 		min_target = lambda_U * sum_t
 
-	def Y_ijt(self, user_i, item_j, time):
-		# Y_ijt = 1 if user_i has a link with item_j at time t, else=0
-		pass
+	def Y_R_ijt(self, user_i, item_j, time):
 
-	def R_ijt(self, user_i, item_j, time):
+		# Y_ijt = 1 if user_i has a link with item_j at time t, else=0
 		# R_ijt = rating preference score of user_i to item_j at time t
-		pass
+		# `type` tb_miduserrelation_selected  # SELECT * FROM tb_miduserrelation
+		sql = """SELECT `type` FROM tb_miduserrelation_selected
+				WHERE `:START_ID`=%s AND `:END_ID`=%s AND `time`="%s" """ % (user_i, item_j, time)
+		self.cursor.execute(sql)
+		res = self.cursor.fetchall()
+		# print res
+		Y_ijt = 0
+		R_ijt = 0
+		if len(res[0]) == 0:
+			Y_ijt = 0
+			R_ijt = 0
+		else:
+			Y_ijt = 1
+			relation_type = res[0][0]
+			if relation_type == 0:
+				R_ijt = 1
+			elif relation_type == 1:
+				R_ijt = 2
+
+		return Y_ijt, R_ijt
+
+
 
 	def V_j(self, item):
 		# V_j = topic of item_j
@@ -96,5 +121,12 @@ class ProfileEvolution:
 
 
 if __name__ == '__main__':
-	Profile = ProfileEvolution()
-	
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-dbpwd", help="Password of database")
+	parser.add_argument("-dbIP", help="IP address of database")
+	args = parser.parse_args()
+	pwd = args.dbpwd
+	dbip = args.dbIP
+
+	Profile = ProfileEvolution(dbip=dbip, dbname='db_weibodata', pwd=pwd, feature_dimension=50)
+	Profile.Y_R_ijt(1227898, 3361644068075147, '2011-11-02-11:18:14')
