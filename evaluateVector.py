2d68a3617faf32c3a1f8631fd36acf6572e42abf
livelifeyiyi <xiaoya_0922@126.com>
2018-10-29 22:22:32 +0800

    add topicWords formalization

--- evaluateVector.py
+++ evaluateVector.py
@@ -1,4 +1,6 @@
 import codecs
+import pandas as pd
+import numpy as np
 
 
 def format_vector(vector_file):
@@ -19,4 +21,33 @@ def format_vector(vector_file):
 					vecFile.write(' '.join(vectors) + '\n')
 					line = inFile.readline().strip('\n').strip('\r')
 
-format_vector("E:\\code\\SN2\\SE-WRL-master\\Clean-Sogou\\vectors_split_sense.bin")
+
+def get_topic_words(topic_word_file, vector_file):
+	"""
+	get the words in topic_word file to help with visualize
+	:param topic_word_file: TopicWord file
+	:return: topicWord.labels; topicWord.vectors
+	"""
+	vectors = pd.read_table(vector_file, sep='\s+', index_col=0, header=None)
+	with codecs.open(topic_word_file, encoding='utf-8', mode='r') as inFile:
+		with codecs.open('topicWord.labels', encoding='utf-8', mode='a+') as labelFile:
+			with codecs.open('topicWord.vectors', encoding='utf-8', mode='a+') as vecFile:
+				topic_words = []
+				line = inFile.readline()
+				i = 0
+				while line:
+					words = line.split(': ')[1]
+					for each_word in words.split():
+						if each_word not in topic_words:
+							topic_words.append(each_word)
+							labelFile.write(each_word + '\n')
+							topic_word_vec = vectors.ix[each_word.encode('utf-8')].values
+							vecFile.write(np.array2string(topic_word_vec, max_line_width=3000)[1:-1] + '\n')
+					line = inFile.readline()
+					i += 1
+					print i
+
+
+wordVec_file = "E:\\code\\SN2\\SE-WRL-master\\Clean-Sogou\\vectors_split_sense.bin"
+topic_word_file = "E:\\code\\SN2\\pDMM-master\\output\\model.filterAllstop.sense.100.topWords"
+get_topic_words(topic_word_file, wordVec_file)
